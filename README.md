b68091043260ce70fa98511c94be78b28e77240f
fdiblen <f.diblen@esciencecenter.nl>
2017-03-17 16:16:59 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+# SPOT Desktop Application
+
+#### **SPOT-Desktop is still under development**
+
+* install **node.js**
+
+* Clone this repository
+  ```bash
+  git clone https://github.com/fdiblen/spot-desktop.git && cd spot-desktop
+  ```
+
+* install dependencies:
+  ```bash
+  npm install
+  ```
+
+* build binaries:
+  ```bash
+  npm run build-all
+  ```
---
+++ main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 1100, height: 800})
+
+  // diable menu bar
+  win.setMenu(null);
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'src/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
