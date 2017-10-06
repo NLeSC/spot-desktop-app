ef9f868249b546a2c96b640bf83501e0dce14dc4
fdiblen <f.diblen@esciencecenter.nl>
2017-10-06 18:36:02 +0200

    * Use SPOT 0.1.0 * update package.json * update .gitignore

--- main.js
+++ main.js
@@ -5,39 +5,40 @@ const url = require('url')
 let win
 
 function createWindow () {
-  win = new BrowserWindow({
-    width: 1400, height: 900,
-    icon: './images/flask.svg'
-//    , fullscreen: true
-  })
+    win = new BrowserWindow({
+      width: 1400, height: 900,
+      icon: './images/flask.svg'
+  //    , fullscreen: true
+    })
 
-  // diable menu bar
-  win.setMenu(null);
+    // diable menu bar
+    // win.setMenu(null);
+  //  win.maximize();
 
-//  win.maximize();
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'src/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'src/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+      win = null
+    })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+      createWindow()
+    }
 })
